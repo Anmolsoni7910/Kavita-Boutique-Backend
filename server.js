a50841e3494c2aa1eddcb7649cceb304123e01c7
anmolsoni7910 <anmolsoni7910@gmail.com>
2024-05-20 02:21:57 +0530

    remove webpack and vercel

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
-import app from './index.js';
-import { mongodbConnect } from './config/db.js';
+import app from './src/index.js';
+import { mongodbConnect } from './src/config/db.js';
 
 const PORT = process.env.PORT || 5454;
 
